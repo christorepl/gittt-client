a08fa9e65f255c70e5698319541bcdf42f58978a
Christopher <chrisobrien416@gmail.com>
2021-01-05 18:49:02 -0500

    wiring up bga auth

--- src/Components/BGAAuth/BGAAuth.js
+++ src/Components/BGAAuth/BGAAuth.js
@@ -33,7 +33,7 @@ export default class BGAAuth extends React.Component {
             headers: {
                 "content-type": "application/x-www-form-urlencoded"
             },
-            body: JSON.stringify(body)
+            body
         }).then(response => {
             if (response.ok) {
                 response.json().then(json => {
