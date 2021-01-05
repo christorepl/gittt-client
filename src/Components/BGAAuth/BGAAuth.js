cb54a5cc1040b153fb36acb7ccae8f7cafff22ce
Christopher <chrisobrien416@gmail.com>
2021-01-05 18:29:25 -0500

    wiring up bga auth

--- src/Components/BGAAuth/BGAAuth.js
+++ src/Components/BGAAuth/BGAAuth.js
@@ -7,11 +7,11 @@ export default class BGAAuth extends React.Component {
         let code = codeStr.substring(6, codeStr.length - 11)
         console.log(code)
         const body = {
-            client_id: "LN1xFTrB6e",
-            client_secret: "17c218619e19b928562296f2edbdc711",
-            code,
-            redirect_uri: "https://get-it-to-the-table.vercel.app/bga-auth",
-            grant_type: "authorization_code"
+            "client_id": "LN1xFTrB6e",
+            "client_secret": "17c218619e19b928562296f2edbdc711",
+            "code" : code,
+            "redirect_uri": "https://get-it-to-the-table.vercel.app/bga-auth",
+            "grant_type": "authorization_code"
         }
         try {
             const response = await fetch("https://api.boardgameatlas.com/oauth/token" ,
