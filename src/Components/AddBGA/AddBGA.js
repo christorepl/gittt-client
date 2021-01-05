440f500467ab817162907a7b60042b74a4b3fb03
Christopher <chrisobrien416@gmail.com>
2021-01-05 16:19:29 -0500

    wiring up bga auth

--- src/Components/AddBGA/AddBGA.js
+++ src/Components/AddBGA/AddBGA.js
@@ -24,8 +24,46 @@ export default class AddBGA extends React.Component {
         return(
             <div className="info">
                 add bga
-                <a href="https://api.boardgameatlas.com/oauth/authorize?response_type=code&client_id=LN1xFTrB6e&redirect_uri=https://get-it-to-the-table.vercel.app/bga-auth&state=null/" target="_blank" rel="noopener noreferrer"><button>add bga button</button></a>
+                <a href="https://api.boardgameatlas.com/oauth/authorize?response_type=code&client_id=LN1xFTrB6e&state=null&redirect_uri=https://get-it-to-the-table.vercel.app/bga-auth/" target="_blank" rel="noopener noreferrer"><button>add bga button</button></a>
             </div>
         )
     }
 }
+
+/*
+import React from 'react'
+import axios from 'axios'
+import API_BASE_URL from '../../config'
+
+export default class AddBGA extends React.Component {
+    // async componentDidMount() {
+    //     try {
+    //         const response = await fetch(API_BASE_URL + '/add-bga')
+    //         const allRes = await response.json()
+    //         alert(allRes)
+    //     } catch (error) {
+    //         console.error(error.message)
+    //     } 
+    // }
+    bgaAuth = async () => {
+        var options = {
+            method: 'GET',
+            url: 'https://api.boardgameatlas.com/oauth/authorize?response_type=code&client_id=LN1xFTrB6e&redirect_uri=https://get-it-to-the-table.vercel.app/bga-auth/',
+            params: {state: null},
+        }
+        axios.request(options).then(function (response) {
+            console.log(response)
+        }).catch(function (error) {
+            console.error(error)
+        })
+    }
+    render() {
+        return(
+            <div className="info">
+                add bga
+                <button onClick={() => this.bgaAuth()}>add bga button</button>
+            </div>
+        )
+    }
+}
+*/
