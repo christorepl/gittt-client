fbaffab1e734b439f5031eaeb11ef930d30759ba
Christopher <chrisobrien416@gmail.com>
2021-01-05 16:05:17 -0500

    wiring up bga auth

--- src/Components/AddBGA/AddBGA.js
+++ src/Components/AddBGA/AddBGA.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import API_BASE_URL from '../../config'
 
 export default class AddBGA extends React.Component {
@@ -12,18 +13,22 @@ export default class AddBGA extends React.Component {
     //     } 
     // }
     bgaAuth = async () => {
-        try {
-            const response = await fetch('https://api.boardgameatlas.com/oauth/authorize?response_type=code&client_id=LN1xFTrB6e&redirect_uri=https://get-it-to-the-table.vercel.app/bga-auth/')
-            const res = await response.json()
-        } catch (error) {
-            console.error(error)
+        var options = {
+            method: 'GET',
+            url: 'https://api.boardgameatlas.com/oauth/authorize?response_type=code&client_id=LN1xFTrB6e&redirect_uri=https://get-it-to-the-table.vercel.app/bga-auth/',
+            params: {state: null},
         }
+        axios.request(options).then(function (response) {
+            console.log(response)
+        }).catch(function (error) {
+            console.error(error)
+        })
     }
     render() {
         return(
             <div className="info">
                 add bga
-                <a href="https://api.boardgameatlas.com/oauth/authorize?response_type=code&client_id=LN1xFTrB6e&redirect_uri=https://get-it-to-the-table.vercel.app/bga-auth/" target="_blank" rel="noopener noreferrer"><button>add bga button</button></a>
+                <button onClick={() => this.bgaAuth()}>add bga button</button>
             </div>
         )
     }
