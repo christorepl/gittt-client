5f6b0c04b749fdb8370cd6988db87077509a6c15
Christopher <chrisobrien416@gmail.com>
2021-01-06 10:51:48 -0500

    wiring up bga auth

--- src/Components/BGAAuth/BGAAuth.js
+++ src/Components/BGAAuth/BGAAuth.js
@@ -117,16 +117,16 @@ export default class BGAAuth extends React.Component {
     }
 }
 
-method: "POST",
-headers: {
-    "content-type": "application/x-www-form-urlencoded"
-},
-form: {
-    client_id,
-    client_secret,
-    redirect_uri,
-    grant_type
-}
+// method: "POST",
+// headers: {
+//     "content-type": "application/x-www-form-urlencoded"
+// },
+// form: {
+//     client_id,
+//     client_secret,
+//     redirect_uri,
+//     grant_type
+// }
 
 
 
@@ -135,88 +135,88 @@ form: {
 
 
 
-{
-    "method": "POST",
-    "header": [
-        {
-            "key": "content-type",
-            "value": "application/x-www-form-urlencoded",
-            "type": "text"
-        }
-    ],
-    "body": {
-        "mode": "urlencoded",
-        "urlencoded": [
-            {
-                "key": "client_id",
-                "value": "LN1xFTrB6e",
-                "type": "text"
-            },
-            {
-                "key": "client_secret",
-                "value": "17c218619e19b928562296f2edbdc711",
-                "type": "text"
-            },
-            {
-                "key": "code",
-                "value": "e8ec199476f64154a582f3278d0914c8781ac762",
-                "type": "text"
-            },
-            {
-                "key": "redirect_uri",
-                "value": "https://get-it-to-the-table.vercel.app/bga-auth/ ",
-                "type": "text"
-            },
-            {
-                "key": "grant_type",
-                "value": "authorization_code",
-                "type": "text"
-            }
-        ],
-        "options": {
-            "raw": {
-                "language": "json"
-            }
-        }
-    },
-    "url": {
-        "raw": "https://api.boardgameatlas.com/oauth/token",
-        "protocol": "https",
-        "host": [
-            "api",
-            "boardgameatlas",
-            "com"
-        ],
-        "path": [
-            "oauth",
-            "token"
-        ],
-        "query": [
-            {
-                "key": "client_id",
-                "value": "LN1xFTrB6e",
-                "disabled": true
-            },
-            {
-                "key": "client_secret",
-                "value": "17c218619e19b928562296f2edbdc711",
-                "disabled": true
-            },
-            {
-                "key": "code",
-                "value": "f719ff21228b7e81bdc6235c9e502a0dbbc26614",
-                "disabled": true
-            },
-            {
-                "key": "redirect_uri",
-                "value": "https://get-it-to-the-table.vercel.app/bga-auth",
-                "disabled": true
-            },
-            {
-                "key": "grant_type",
-                "value": "authorization_code",
-                "disabled": true
-            }
-        ]
-    }
-}
+// {
+//     "method": "POST",
+//     "header": [
+//         {
+//             "key": "content-type",
+//             "value": "application/x-www-form-urlencoded",
+//             "type": "text"
+//         }
+//     ],
+//     "body": {
+//         "mode": "urlencoded",
+//         "urlencoded": [
+//             {
+//                 "key": "client_id",
+//                 "value": "LN1xFTrB6e",
+//                 "type": "text"
+//             },
+//             {
+//                 "key": "client_secret",
+//                 "value": "17c218619e19b928562296f2edbdc711",
+//                 "type": "text"
+//             },
+//             {
+//                 "key": "code",
+//                 "value": "e8ec199476f64154a582f3278d0914c8781ac762",
+//                 "type": "text"
+//             },
+//             {
+//                 "key": "redirect_uri",
+//                 "value": "https://get-it-to-the-table.vercel.app/bga-auth/ ",
+//                 "type": "text"
+//             },
+//             {
+//                 "key": "grant_type",
+//                 "value": "authorization_code",
+//                 "type": "text"
+//             }
+//         ],
+//         "options": {
+//             "raw": {
+//                 "language": "json"
+//             }
+//         }
+//     },
+//     "url": {
+//         "raw": "https://api.boardgameatlas.com/oauth/token",
+//         "protocol": "https",
+//         "host": [
+//             "api",
+//             "boardgameatlas",
+//             "com"
+//         ],
+//         "path": [
+//             "oauth",
+//             "token"
+//         ],
+//         "query": [
+//             {
+//                 "key": "client_id",
+//                 "value": "LN1xFTrB6e",
+//                 "disabled": true
+//             },
+//             {
+//                 "key": "client_secret",
+//                 "value": "17c218619e19b928562296f2edbdc711",
+//                 "disabled": true
+//             },
+//             {
+//                 "key": "code",
+//                 "value": "f719ff21228b7e81bdc6235c9e502a0dbbc26614",
+//                 "disabled": true
+//             },
+//             {
+//                 "key": "redirect_uri",
+//                 "value": "https://get-it-to-the-table.vercel.app/bga-auth",
+//                 "disabled": true
+//             },
+//             {
+//                 "key": "grant_type",
+//                 "value": "authorization_code",
+//                 "disabled": true
+//             }
+//         ]
+//     }
+// }
