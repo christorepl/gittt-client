5248d89bd5b7e42d9d5d6a69bd0240c359ac5226
Christopher <chrisobrien416@gmail.com>
2021-01-06 17:31:01 -0500

    wiring up bga auth

--- src/Components/BGAAuth/BGAAuth.js
+++ src/Components/BGAAuth/BGAAuth.js
@@ -8,27 +8,29 @@ export default class BGAAuth extends React.Component {
         let codeStr = this.props.location.search
         console.log(codeStr)
         let code = codeStr.substring(6, codeStr.length)
-        console.log(code)
-        let client_id= "LN1xFTrB6e"
-        let client_secret = "17c218619e19b928562296f2edbdc711"
-        let redirect_uri= "https://get-it-to-the-table.vercel.app/bga-auth/"
-        let grant_type ="authorization_code"
-        fetch('https://api.boardgameatlas.com/oauth/token', {
-            method: "POST",
-            headers: {
-                "content-type": "application/x-www-form-urlencoded"
-            },
-            body: 
-                `client_id=${client_id}&client_secret=${client_secret}&redirect_uri=${redirect_uri}&grant_type=${grant_type}&code=${code}`
-        }).then(response => {
-            if (response.ok) {
-                response.json().then(json => {
-                    console.log(json)
-                })
-            }
-        }).catch(function(error) {
-            console.log(error)
-        })
+        //send code with the POST request to the API
+        try {
+            const response = await fetch(API_BASE_URL + "/bga-auth/", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: {
+                    code
+                }
+            }).then(response => {
+                if (response.ok) {
+                    response.json().then(json =>{
+                        console.log('succes ', json)
+                    })
+                }
+            }).catch(function(error) {
+                console.log('local catch error ', error)
+            })
+        } catch (error) {
+            console.error(error)
+        }
+
 
     }
     render() {
