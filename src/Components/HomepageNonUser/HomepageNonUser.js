38ab9579ca4d656914c2cb4e7daf0589668c483c
Christopher <chrisobrien416@gmail.com>
2021-02-09 12:37:57 -0500

    added logo and changed bg color

--- src/Components/HomepageNonUser/HomepageNonUser.js
+++ src/Components/HomepageNonUser/HomepageNonUser.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import AppContext from '../../Context/AppContext'
+import gitttLogo from '../../img/gittt-logo.png'
 
 export default class HomepageNonUser extends React.Component {
     static contextType = AppContext
@@ -9,12 +10,19 @@ export default class HomepageNonUser extends React.Component {
         return(
             <>
             <div className="info">
-    
+                <img src={gitttLogo} alt='Get it to the Table logo'/>
                 <p>Do you have so many tabletop games in your collection that it's a hassle to choose a game? Or maybe you want your playgroup to come to a consensus on a new game to purchase? That's where Get it to the Table comes in. You and your playgroup can form a group and match on games! Once everyone swipes right on a game, the game will count as a match and everyone can see the list of matched games!</p>
     
-                <h3>Step 1:</h3>
-                
-                <a href="https://www.boardgameatlas.com/" target="_blank" rel="noopener noreferrer">BGA lists</a>
+                <h4>Step 1:</h4>
+                <p>Add contacts to your account</p>
+                <h4>Step 2:</h4>
+                <p>Add a <a href="https://www.boardgameatlas.com/" target="_blank" rel="noopener noreferrer">BGA list</a> to your account</p>
+                <h4>Step 3:</h4>
+                <p>Create a group with your contacts</p>
+                <h4>Step 4:</h4>
+                <p>Add a BGA list to a group</p>
+                <h4>Step 5:</h4>
+                <p>Get to swiping and wait for matches! Matches are shown a group's swiping page.</p>
             <Link to='/create-account'>Create account</Link>
             <Link to='/login'>Login</Link>
             </div>
