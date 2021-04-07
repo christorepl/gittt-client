0694a2a592cccb224a88e6d852f6764e69e10bea
Christopher <chrisobrien416@gmail.com>
2021-04-07 10:34:08 -0400

    screenshot

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,39 +1,52 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom'
-import AppContext from '../../Context/AppContext'
+import React from "react";
+import { Redirect } from "react-router-dom";
+import AppContext from "../../Context/AppContext";
 
 export default class Login extends React.Component {
-    static contextType = AppContext
+  static contextType = AppContext;
 
-    render() {
-
-       return (
-            <>
-            {this.context.isAuthenticated
-            ?
-            <Redirect to="/home"/>
-            :
-            <div className="user-forms">
-                <form className="user-form" onSubmit={(e) => this.context.onSubmitLogin(e)}>
-                <label htmlFor="email">E-mail Address:</label>
-                <br/>
-                <input type="email" name="email" required onChange={e => this.context.setEmail(e.target.value)}/>
-                <br/>
-                <label htmlFor="password">Password:</label>
-                <br/>
-                <input type="password" name="password" required onChange={e => this.context.setPassword(e.target.value)}/>
-                <br/>
-                <button type="submit">Login</button>
+  render() {
+    return (
+      <>
+        {this.context.isAuthenticated ? (
+          <Redirect to="/home" />
+        ) : (
+          <div className="user-forms">
+            <form
+              className="user-form"
+              onSubmit={(e) => this.context.onSubmitLogin(e)}
+            >
+              <label htmlFor="email">E-mail Address:</label>
+              <br />
+              <input
+                type="email"
+                name="email"
+                required
+                onChange={(e) => this.context.setEmail(e.target.value)}
+              />
+              <br />
+              <label htmlFor="password">Password:</label>
+              <br />
+              <input
+                type="password"
+                name="password"
+                required
+                onChange={(e) => this.context.setPassword(e.target.value)}
+              />
+              <br />
+              <button type="submit">Login</button>
+              <footer>
                 <p>For testing, try using the these credentials:</p>
                 <p>E-mail: test@test.com - Password: test</p>
                 <p>E-mail: jimmy@test.com - Password: test</p>
                 <p>E-mail: johnny@test.com - Password: test</p>
                 <p>E-mail: bobby@test.com - Password: test</p>
                 <p>E-mail: joey@test.com - Password: test</p>
-                </form>
-            </div>
-            }
-            </>
-        )
-    }
+              </footer>
+            </form>
+          </div>
+        )}
+      </>
+    );
+  }
 }
